f078538d57313afc4c604cf8c954f480446ea5cc
kleinsimon <sklein8@googlemail.com>
2013-08-10 18:35:42 +0200

    init

---
+++ SEMDock/Classes.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Text;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using CorelDRAW;
+using System.Runtime.Serialization;
+
+namespace SEMTools4CD
+{
+    public class TIFFitem
+    {
+        string _path, _filename;
+        double _calibration = 1d;
+
+        public string path { get { return _path; } set { _path = value; } }
+        public string filename { get { return _filename; } set { _filename = value; } }
+        public double calibration { get { return _calibration; } set { _calibration = value; } }
+
+        public TIFFitem(string file)
+        {
+            if (File.Exists(file))
+            {
+                path = file;
+                filename = Path.GetFileName(file);
+
+                StreamReader FS = new StreamReader(file);
+                string line = "";
+                string cstring = "";
+
+                while ((line = FS.ReadLine()) != null)
+                {
+                    if (line.Contains("AP_IMAGE_PIXEL_SIZE"))
+                    {
+                        cstring = FS.ReadLine().Split('=')[1];
+                        break;
+                    }
+                }
+
+                if (cstring != "") calibration = parseCalibMicron(cstring);
+            }
+
+        }
+
+        private double parseCalibMicron(string Cstring)
+        {
+            double val = 0d;
+            string[] tmp;
+
+            NumberFormatInfo NF = new NumberFormatInfo();
+            NF.NumberDecimalSeparator = ".";
+            NF.NumberGroupSeparator = "";
+
+            tmp = Cstring.Trim().Split(' ');
+            if (double.TryParse(tmp[0], NumberStyles.Any, NF, out val))
+            {
+                tmp[1] = tmp[1].Trim();
+
+                switch (tmp[1])
+                {
+                    case "nm": val = val / 1000d;
+                        break;
+                    case "mm": val = val * 1000d;
+                        break;
+                    case "pm": val = val / 1000000d;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return val;
+        }
+    }
+
+    [Serializable,DataContract]
+    public class semImageData
+    {
+        [DataMember(Name = "ULtext")]
+        private string _ULtext = "";
+        [DataMember(Name = "URtext")]
+        private string _URtext = "";
+        [DataMember(Name = "BLtext")]
+        private string _BLtext = "";
+        [DataMember(Name = "BarText")]
+        private string _BarText = "";
+        [DataMember(Name = "FileName")]
+        private string _filename = "";
+        [DataMember(Name = "FontSize")]
+        private float _FontSize = 10f;
+        [DataMember(Name = "BarWidth")]
+        private float _BarWidth = 1.5f;
+        [DataMember(Name = "BorderWidth")]
+        private float _BorderWidth = 1.5f;
+        [DataMember(Name = "Width")]
+        private float _Width = 11f;
+        [DataMember(Name = "Height")]
+        private float _Height = 0f;
+        [DataMember(Name = "BarLength")]
+        private double _BarLength = 3f;
+        [DataMember(Name = "Calibration")]
+        private double _Calibration = 1d;
+        [DataMember(Name = "Mode")]
+        private int _Mode = 1;
+        [DataMember(Name = "TextBold")]
+        private bool? _TextBold = false;
+        [DataMember(Name = "BarMinWidth")]
+        private float _BarMinWidth = 3f;
+        [DataMember(Name = "BarMaxWidth")]
+        private float _BarMaxWidth = 5f;
+
+        public string ULtext { get { return _ULtext; } set { _ULtext = value; } }
+        public string URtext { get { return _URtext; } set { _URtext = value; } }
+        public string BLtext { get { return _BLtext; } set { _BLtext = value; } }
+        public string BarText { get { return _BarText; } set { _BarText = value; } }
+        public string filename { get { return _filename; } set { _filename = value; } }
+        public float FontSize { get { return _FontSize; } set { _FontSize = value; } }
+        public float BarWidth { get { return _BarWidth; } set { _BarWidth = value; } }
+        public float BorderWidth { get { return _BorderWidth; } set { _BorderWidth = value; } }
+        public float Width { get { return _Width; } set { _Width = value; } }
+        public float Height { get { return _Height; } set { _Height = value; } }
+        public double BarLength { get { return _BarLength; } set { _BarLength = value; } }
+        public double Calibration { get { return _Calibration; } set { _Calibration = value; } }
+        public int Mode { get { return _Mode; } set { _Mode = value; } }
+        public bool? TextBold { get { return _TextBold; } set { _TextBold = value; } }
+        public float BarMinWidth { get { return _BarMinWidth; } set { _BarMinWidth = value; } }
+        public float BarMaxWidth { get { return _BarMaxWidth; } set { _BarMaxWidth = value; } }
+
+
+        public override string ToString()
+        {
+            System.Runtime.Serialization.Json.DataContractJsonSerializer js = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(semImageData));
+            Stream s = new MemoryStream();
+            js.WriteObject(s, this);
+            s.Position = 0;
+            StreamReader sr = new StreamReader(s);
+            return sr.ReadToEnd();
+        }
+
+        public static semImageData FromString(string JsonData)
+        {
+            System.Runtime.Serialization.Json.DataContractJsonSerializer js = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(semImageData));
+            byte[] bA = Encoding.ASCII.GetBytes(JsonData);
+            Stream s = new MemoryStream(bA);
+            s.Position = 0;
+            semImageData tmp = (semImageData)js.ReadObject(s);
+            return tmp;
+        }
+
+        public semImageData Clone()
+        {
+            return semImageData.FromString(this.ToString());
+        }
+    }
+
+    public class semImage
+    {
+        private CorelDRAW.Application CDWin;
+        public CorelDRAW.Shape imgShape;
+        public semImageData imgData;
+
+        public semImage(CorelDRAW.Application _CDWin, semImageData Data, CorelDRAW.Shape _imgShape)
+        {
+            CDWin = _CDWin;
+            imgShape = _imgShape;
+            imgData = Data;
+        }
+
+        public void calcScale()
+        {
+            double[] res = new double[2];
+            double OffsetResolution = CDWin.ConvertUnits(
+                imgShape.Bitmap.SizeWidth / imgShape.SizeWidth,
+                cdrUnit.cdrCentimeter,
+                CDWin.ActiveDocument.Unit);
+
+            double minVal = imgData.BarMinWidth* OffsetResolution * imgData.Calibration;
+            double pot = Math.Pow(10d, Math.Floor(Math.Log10(minVal)));
+
+            for (int i = 1; i <= 19; i++)
+            {
+                double l, z;
+
+                z = i * pot;
+                l = z / (OffsetResolution * imgData.Calibration);
+
+                if (l >= imgData.BarMinWidth && l <= imgData.BarMaxWidth)
+                {
+                    imgData.BarLength = l;
+                    imgData.BarText = getScaleText( z);
+                    return;
+                }
+                else
+                {
+                    z *= 10;
+                    l = z / (OffsetResolution * imgData.Calibration);
+                    if (l >= imgData.BarMinWidth && l <= imgData.BarMaxWidth)
+                    {
+                        imgData.BarLength = l;
+                        imgData.BarText = getScaleText(z);
+                        return;
+                    }
+                }
+            }
+            imgData.BarLength = 2d;
+            imgData.BarText = "Error";
+            return;
+        }
+
+        private string getScaleText(double scale)
+        {
+            string res = "";
+
+            if (scale >= 1000000000d) res = (scale / 1000000000d).ToString() + " km";
+            else if (scale >= 1000000d) res = (scale / 1000000d).ToString() + " m";
+            else if (scale >= 10000d) res = (scale / 10000d).ToString() + " cm";
+            else if (scale >= 1000d) res = (scale / 1000d).ToString() + " mm";
+            else if (scale <= 0.001d) res = (scale * 1000000d).ToString() + " pm";
+            else if (scale <= 1d) res = (scale * 1000d).ToString() + " nm";
+            else res = scale.ToString() + " µm";
+
+            return res;
+        }
+    }
+
+    enum PropID
+    {
+        calibration = 1,
+        TextOL = 2,
+        TextOR = 3,
+        TextUL = 4,
+        Length = 5,
+        Text = 6,
+        lineW = 7,
+        lineBW = 8,
+        TextBold = 9,
+        TextSize = 10,
+        width = 11,
+        height = 12,
+        Mode = 13,
+        isBalkenGroup = 14,
+        filename = 15,
+    }
+}
---
+++ SEMDock/SEMDock.xaml.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using CorelDRAW;
+using VGCore;
+
+namespace SEMTools4CD
+{
+    enum ShapeProperties
+    {
+        isLatexObject = 1,
+        latexText = 2,
+        latexTemplate = 3,
+    }
+
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class SEMDock : UserControl
+    {
+        CorelDRAW.Application CDWin;
+        List<semImage> editShapes = new List<semImage>();
+        semImageData currentSettings;
+        semImageData Settings;
+        bool locked = false;
+        string[] allowedExt = { ".tiff", ".tif" };
+        ObservableCollection<TIFFitem> _TiffItems = new ObservableCollection<TIFFitem>();
+        public ObservableCollection<TIFFitem> TiffItems { get { return _TiffItems; } }
+
+        public SEMDock()
+        {
+            InitializeComponent();
+            initWin();
+        }
+
+        public SEMDock(object app)
+        {
+            InitializeComponent();
+            initWin();
+            CDWin = (CorelDRAW.Application)app;
+            CDWin.SelectionChange += CDWin_SelectionChange;
+
+        }
+
+        void initWin()
+        {
+            tiffList.ItemsSource = TiffItems;
+            try
+            {
+                //currentSettings = new semImageData();
+                Settings = semImageData.FromString(Properties.Settings.Default.LastData);
+            }
+            catch
+            {
+                Settings = new semImageData();
+            }
+            currentSettings = Settings;
+            this.DataContext = currentSettings;
+        }
+
+        void CDWin_SelectionChange()
+        {
+            if (locked) return;
+            if (CDWin.ActiveSelection.Shapes.Count == 1 && CDWin.ActiveSelection.Shapes[1].Properties.Exists("semItem", 0))
+            {
+                try
+                {
+                    currentSettings = semImageData.FromString(CDWin.ActiveSelection.Shapes[1].Properties["semItem", 1]);
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                currentSettings = Settings;
+            }
+        }
+
+        private void ListView_DragEnter_1(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Move;
+            }
+        }
+
+        private void tiffList_Drop(object sender, DragEventArgs e)
+        {
+            foreach (string file in ((string[])e.Data.GetData(DataFormats.FileDrop, true)))
+            {
+                if (allowedExt.Contains(Path.GetExtension(file)))
+                {
+                    _TiffItems.Add(new TIFFitem(file));
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".tif";
+            dlg.Filter = "Tiff-File|*.tif;*.tiff";
+            dlg.Multiselect = true;
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                foreach (string file in dlg.FileNames)
+                {
+                    _TiffItems.Add(new TIFFitem(file));
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            _TiffItems.Clear();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            locked = true;
+            try
+            {
+                if (tabMode.SelectedIndex == 0 && _TiffItems.Count > 0)
+                {
+                    editShapes.Clear();
+                    foreach (TIFFitem item in TiffItems)
+                    {
+                        CDWin.ActiveDocument.ClearSelection();
+                        CDWin.ActiveDocument.ActiveLayer.Import(item.path);
+                        CorelDRAW.Shape impShape = CDWin.ActiveSelection.Shapes[1];
+                        impShape.AlignToPoint(CorelDRAW.cdrAlignType.cdrAlignVCenter, CDWin.Application.ActiveWindow.ActiveView.OriginX, CDWin.Application.ActiveWindow.ActiveView.OriginY);
+                        impShape.AlignToPoint(CorelDRAW.cdrAlignType.cdrAlignHCenter, CDWin.Application.ActiveWindow.ActiveView.OriginX, CDWin.Application.ActiveWindow.ActiveView.OriginY);
+                        semImage imp = new semImage(CDWin, currentSettings.Clone(), impShape);
+                        imp.imgData.filename = Path.GetFileName(item.path);
+                        imp.imgData.Calibration = item.calibration;
+                        imp.imgData.Mode = (int)CreateMode.Calib;
+                        editShapes.Add(imp);
+                    }
+                    _TiffItems.Clear();
+                }
+                else if (tabMode.SelectedIndex > 0 && CDWin.ActiveSelection.Shapes.Count > 0)
+                {
+                    foreach (Shape sh in CDWin.ActiveSelection.Shapes)
+                    {
+                        if (sh.Properties.Exists("semItem", 0))
+                        {
+                            foreach (Shape sh_child in sh.PowerClip.Shapes)
+                            {
+                                if (sh_child.Name == "semItemContent")
+                                {
+                                    editShapes.Add(new semImage(CDWin, currentSettings.Clone(), sh_child));
+                                }
+                                else
+                                {
+                                    sh_child.Delete();
+                                }
+                            }
+                            sh.PowerClip.ExtractShapes();
+                            sh.Delete();
+                        }
+                        else
+                        {
+                            editShapes.Add(new semImage(CDWin, currentSettings.Clone(), sh));
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No image selected");
+                    return;
+                }
+
+                CDWin.ActiveDocument.BeginCommandGroup("semItem");
+                CDWin.Application.Optimization = true;
+
+                foreach (semImage img in editShapes)
+                {
+                    decorateShape(img);
+                }
+                editShapes.Clear();
+                CDWin.ActiveDocument.EndCommandGroup();
+                CDWin.Application.Optimization = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            locked = false;
+        }
+
+        private void decorateShape(semImage curShape)
+        {
+            ShapeRange SR = new ShapeRange();
+            ShapeRange TextShapes = new ShapeRange();
+            Shape Brect, Lrect, Wline, Ttext, Gr;
+            double LmarginH, LmarginV, TmarginH, TmarginV, Theight;
+            double Lwidth, Wwidth, Wheight, Lheight;
+            double Left, Bottom, Width, Height;
+            double cimgratio;
+            bool NoBar = false;
+            Shape s = curShape.imgShape;
+            semImageData d = curShape.imgData;
+            Color White, Black;
+
+            White = new Color();
+            Black = new Color();
+            Black.CMYKAssign(0, 0, 0, 100);
+            White.CMYKAssign(0, 0, 0, 0);
+
+
+            Left = s.LeftX;
+            Bottom = s.BottomY;
+
+            cimgratio = s.SizeHeight / s.SizeWidth;
+
+            if (d.Width == 0d && d.Height == 0d)
+            {
+                Width = s.SizeWidth;
+                Height = s.SizeHeight;
+            }
+            else if (d.Width == 0d)
+            {
+                Height = cmToUnit(d.Height);
+                Width = Height / cimgratio;
+            }
+            else if (d.Height == 0d)
+            {
+                Width = cmToUnit(d.Width);
+                Height = Width * cimgratio;
+            }
+            else
+            {
+                Width = cmToUnit(d.Width);
+                Height = cmToUnit(d.Height);
+            }
+            double newRatio = Height / Width;
+
+            if (newRatio <= cimgratio)
+            {
+                s.SizeWidth = Width;
+                s.SizeHeight = Width * cimgratio;
+            }
+            else
+            {
+                s.SizeHeight = Height;
+                s.SizeWidth = Height / cimgratio;
+            }
+            s.LeftX = Left;
+            s.BottomY = Bottom;
+
+            if (d.Mode == (int)CreateMode.Calib)
+            {
+                curShape.calcScale();
+            }
+            else if (d.Width == 0 || d.BarText == "")
+            {
+                NoBar = true;
+            }
+
+            Lwidth = cmToUnit(curShape.imgData.BarLength);
+
+            Lheight = ptToUnit(8);
+            LmarginH = ptToUnit(8);
+            LmarginV = ptToUnit(4);
+            TmarginH = ptToUnit(4);
+            TmarginV = ptToUnit(4);
+
+            Theight = ptToUnit(d.FontSize);
+            Wwidth = Lwidth + 2 * LmarginH;
+            Wheight = Lheight + LmarginV + TmarginV;
+
+            Brect = CDWin.ActiveLayer.CreateRectangle2(Left, Bottom, Width, Height);
+            Brect.Outline.Color = Black;
+            Brect.Outline.Width = ptToUnit(d.BorderWidth);
+
+            s.Name = "semItemContent";
+            s.AddToPowerClip(Brect, CorelDRAW.cdrTriState.cdrFalse);
+
+            if (!NoBar)
+            {
+                Lrect = CDWin.ActiveLayer.CreateRectangle2(Left + Width - Wwidth, Bottom, Wwidth, Wheight + Theight);
+                Wline = CDWin.ActiveLayer.CreateLineSegment(Left + Width - LmarginH - Lwidth, Bottom + Wheight / 2, Left + Width - LmarginH, Bottom + Wheight / 2);
+                Ttext = CDWin.ActiveLayer.CreateArtisticText(Left + Width - Wwidth / 2, Bottom + Wheight / 2 + TmarginV, d.BarText, Alignment: CorelDRAW.cdrAlignment.cdrCenterAlignment, Size: d.FontSize);
+
+                Ttext.Text.Story.Bold = d.TextBold == true;
+                Lrect.Outline.Width = 0;
+                Lrect.Fill.ApplyUniformFill(White);
+
+                Wline.Outline.Width = ptToUnit(d.BarWidth);
+                Wline.Outline.Color = Black;
+                Wline.Outline.EndArrow = CDWin.ArrowHeads[59];
+                Wline.Outline.StartArrow = CDWin.ArrowHeads[59];
+                SR.Add(Lrect);
+                SR.Add(Wline);
+                SR.Add(Ttext);
+                Gr = SR.Group();
+                Gr.AddToPowerClip(Brect, CorelDRAW.cdrTriState.cdrFalse);
+            }
+
+            if (d.ULtext.Trim() != "")
+            {
+                Shape back, text;
+                text = CDWin.ActiveLayer.CreateArtisticText(Left + TmarginH, Bottom + Height - Theight - TmarginV, d.ULtext, Alignment: CorelDRAW.cdrAlignment.cdrRightAlignment, Size: d.FontSize);
+                back = CDWin.ActiveLayer.CreateRectangle2(Left, Bottom + Height - Theight - 2 * TmarginV, text.SizeWidth + 2 * TmarginH, Theight + 2 * TmarginV);
+                back.Fill.ApplyUniformFill(White);
+                back.Outline.Width = 0;
+                back.OrderBackOf(text);
+                text.Text.Story.Bold = d.TextBold == true;
+                TextShapes.Add(text);
+                TextShapes.Add(back);
+            }
+
+            if (d.URtext != "")
+            {
+                Shape back, text;
+                text = CDWin.ActiveLayer.CreateArtisticText(Left + Width - TmarginH, Bottom + Height - Theight - TmarginV, d.URtext, Alignment: CorelDRAW.cdrAlignment.cdrRightAlignment, Size: d.FontSize);
+                back = CDWin.ActiveLayer.CreateRectangle2(Left + Width - TextShapes[TextShapes.Count].SizeWidth - 2 * TmarginH, Bottom + Height - Theight - 2 * TmarginV, TextShapes[TextShapes.Count].SizeWidth + 2 * TmarginH, Theight + 2 * TmarginV);
+                back.Fill.ApplyUniformFill(White);
+                back.Outline.Width = 0;
+                back.OrderBackOf(text);
+                text.Text.Story.Bold = d.TextBold == true;
+                TextShapes.Add(text);
+                TextShapes.Add(back);
+            }
+
+            if (d.BLtext != "")
+            {
+                Shape back, text;
+                text = CDWin.ActiveLayer.CreateArtisticText(Left + TmarginH, Bottom + TmarginV, d.BLtext, Alignment: CorelDRAW.cdrAlignment.cdrRightAlignment, Size: d.FontSize);
+                back = CDWin.ActiveLayer.CreateRectangle2(Left, Bottom, TextShapes[TextShapes.Count].SizeWidth + 2 * TmarginH, Theight + 2 * TmarginV);
+                back.Fill.ApplyUniformFill(White);
+                back.Outline.Width = 0;
+                back.OrderBackOf(text);
+                text.Text.Story.Bold = d.TextBold == true;
+                TextShapes.Add(text);
+                TextShapes.Add(back);
+            }
+
+            TextShapes.AddToPowerClip(Brect, CorelDRAW.cdrTriState.cdrFalse);
+
+            Brect.Properties["semItem", 0] = true;
+            Brect.Properties["semItem", 1] = curShape.imgData.ToString();
+            if (d.filename != "") Brect.Name = d.filename;
+
+            CDWin.ActiveWindow.Refresh();
+            CDWin.Application.Refresh();
+            CDWin.ActiveDocument.ClearSelection();
+
+            Brect.Selected = true;
+        }
+
+        double cmToUnit(double centimeter)
+        {
+            return CDWin.ConvertUnits(centimeter, CorelDRAW.cdrUnit.cdrCentimeter, CDWin.ActiveDocument.Unit);
+        }
+
+        double ptToUnit(double centimeter)
+        {
+            return CDWin.ConvertUnits(centimeter, CorelDRAW.cdrUnit.cdrPoint, CDWin.ActiveDocument.Unit);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.LastData = currentSettings.ToString();
+
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    enum CreateMode
+    {
+        Tiff = 0,
+        Calib = 1,
+        Length = 2,
+    }
+}
